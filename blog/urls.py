1bd6eaf53565ceb718fab5cfd455340986e722d6
shahramsamar <shahramsamar2010@gmail.com>
2024-08-28 16:36:58 +0330

    added post-detain in blog

--- blog/urls.py
+++ blog/urls.py
@@ -19,7 +19,8 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name="post-list"),
+    path('', views.PostListView.as_view(), name="post-list"),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name="post-detail"),
 
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from blog.models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 
 # Create your views here.
 
@@ -9,9 +9,16 @@ from django.views.generic import ListView
 '''
 this is for show list of post 
 '''    
-class PostList(ListView):
+class PostListView(ListView):
     model = Post
     template_name = "blog/post-list.html"  
     context_object_name = "posts"
     ordering = ("-id")
     # paginate_by = 1
+
+'''
+this is for show detail of post 
+'''  
+class PostDetailView(DeleteView):
+    model = Post
+    template_name = "blog/post-detail.html"            
