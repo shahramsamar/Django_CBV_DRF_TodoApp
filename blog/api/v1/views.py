ed5b617e01507ec665a037300dcad5a789b7c096
shahramsamar <shahramsamar2010@gmail.com>
2024-12-13 09:30:34 +0330

    status code for show page not found

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -3,6 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from blog.api.v1.serializer import PostSerializer
 from ...models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+ 
+
 
 @api_view()
 def post_list(request):
@@ -10,8 +14,15 @@ def post_list(request):
 
 @api_view()
 def post_detail(request, id):
-    post = Post.objects.get(pk=id)
-    # print(post.__dict__)
+    # try:
+    #     post = Post.objects.get(pk=id)
+    #     # print(post.__dict__)
+    #     serializer = PostSerializer(post)
+    #     # print(serializer.__dict__)
+    #     return Response(serializer.data)
+    # except Post.DoesNotExist:
+    #     # return Response({'detail':'post dose not exist'},status=404)
+    #     return Response({'detail':'post dose not exist'},status=status.HTTP_404_NOT_FOUND)
+    post = get_object_or_404(Post, pk=id)
     serializer = PostSerializer(post)
-    # print(serializer.__dict__)
     return Response(serializer.data)
