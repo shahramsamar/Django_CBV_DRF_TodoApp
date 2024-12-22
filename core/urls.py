ed50b9bba6aabe660444477fd2f9e76cd2f76e70
shahramsamar <shahramsamar2010@gmail.com>
2024-12-22 22:59:37 +0330

    added url for api documents

--- core/urls.py
+++ core/urls.py
@@ -20,12 +20,16 @@ from django.urls import path, include
 from core import settings
 from django.conf.urls.static import static
 
+from rest_framework.documentation import include_docs_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('',include('blog.urls')),
     path('api-auth/',include('rest_framework.urls')),
+    path('api-docs/',include_docs_urls(title="api-sample")),
+
 
 ]
 
