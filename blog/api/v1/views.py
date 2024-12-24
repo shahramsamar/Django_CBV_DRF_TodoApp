ccc7eb2896e43075767b025cace747c7618b3b09
shahramsamar <shahramsamar2010@gmail.com>
2024-12-24 09:48:45 +0330

    added IsOwnerOrReadOnly to permitions

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -4,12 +4,13 @@ from blog.api.v1.serializer import PostSerializer
 from ...models import Post
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
+from blog.api.v1.permission import IsOwnerOrReadOnly
 
 
 
 #  """"""""""""""""""""""""""(ModelViewSet in CBV)"""""""""""""""""""""""""""""""""""""""
 class PostModelViewSet(viewsets.ModelViewSet):
     """ getting a CRUD for posts"""
-    permission_classes =[IsAuthenticatedOrReadOnly]
+    permission_classes =[IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
