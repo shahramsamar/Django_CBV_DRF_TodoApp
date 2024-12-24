1163f4fc3c267c5b4900246693c029bd2947600c
shahramsamar <shahramsamar2010@gmail.com>
2024-12-24 10:16:54 +0330

    added SearchFilter and OrderingFilter

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from blog.api.v1.permission import IsOwnerOrReadOnly
 from blog.api.v1.paginations import DefaultPagination
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 #  """"""""""""""""""""""""""(ModelViewSet in CBV)"""""""""""""""""""""""""""""""""""""""
@@ -17,3 +18,7 @@ class PostModelViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.filter(status=True)
 
     pagination_class = DefaultPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter] 
+    filterset_fields = {'status':['exact']}
+    # Search_fields =['title']
+    ordering_fields = ['created_date']
