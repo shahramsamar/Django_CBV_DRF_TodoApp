14d7d3c5ac7fe233774724503f64c8d05e965f11
shahram samar <shahramsamar2010@gmail.com>
2025-02-23 21:28:36 +0330

    set default folder for api in accounts

--- core/urls.py
+++ core/urls.py
@@ -47,9 +47,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('accounts/',include('accounts.urls')),
-    # path("accounts/", include("django.contrib.auth.urls")),
-
+    path('accounts/',include('accounts.urls')),
     path('',include('blog.urls')),
     path('api-auth/',include('rest_framework.urls')),
     # path('api-docs/',include_docs_urls(title="api-sample")),
