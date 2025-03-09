9940e65e99d0c17a228f75f7b9bafaf78642a154
shahram <shahramsamar2010@gmail.com>
2025-03-09 15:04:16 +0400

    updated

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static/"
 
-STATICFILES_DIR = [BASE_DIR / "static/"]
+STATICFILES_DIR = [BASE_DIR / "staticfiles/"]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media/"
